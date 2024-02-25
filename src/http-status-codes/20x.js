7d2a4d991fdfa9c3d98f1f9b27b82b9700d6657b
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-02-25 12:35:50 -0300

    doc: document new http status codes

--- src/http-status-codes/20x.js
+++ src/http-status-codes/20x.js
@@ -1,7 +1,7 @@
 export const HttpCodes20x = [
   {
     title: "200 - OK",
-    description: "The server has accepted and processed successful your request",
+    description: "The server has accepted and processed successful your request.",
     explanation:
       "When the server has received and processed correctly your request" +
       " it will respond with a 200 Status Code. \n\n" +
@@ -9,4 +9,43 @@ export const HttpCodes20x = [
       " is in body. And if a POST was made could means that your data was accepted correctly."
   },
 
+  {
+    title: "201 - Created",
+    description: "A request has been accepted and a resource was created.",
+    explanation: "If you are trying to POST a resource to a server, it will" +
+      " answer you with this code when successfully created a new resource.",
+  },
+
+  {
+    title: "202 - Accepted",
+    description: "Request accepted for processing, but not completed yet.",
+    explanation: "You made a request to the server that was accepted, but" +
+     " not completed yet. You should check on later for more status.",
+  },
+
+  {
+    title: "203 - Non-Authoritative Information",
+    description: "Received modified version by proxy",
+    explanation: "The server answering your request received a 200 - OK from" +
+      " the original server, but it's giving you a modified version of the" +
+      " original response."
+  },
+
+  {
+    title: "204 - No Content",
+    description: "The server response is empty",
+    explanation: "Your request was made and processed successfully." +
+     " However, the server did not expect to send you anything. Therefore," +
+     " you'll receive a NO CONTENT response.",
+  },
+
+  {
+    title: "205 - Reset Content",
+    description: "",
+    explanation: ""
+  },
+  { title: "206", description: "", explanation: "" },
+  { title: "207", description: "", explanation: "" },
+  { title: "208", description: "", explanation: "" },
+  { title: "226", description: "", explanation: "" },
 ];
---
+++ src/http-status-codes/30x.js
@@ -0,0 +1,11 @@
+export const HttpCodes30x = [
+  { title: "300", description: "", explanation: "" },
+  { title: "301", description: "", explanation: "" },
+  { title: "302", description: "", explanation: "" },
+  { title: "303", description: "", explanation: "" },
+  { title: "304", description: "", explanation: "" },
+  { title: "305", description: "", explanation: "" },
+  { title: "306", description: "", explanation: "" },
+  { title: "307", description: "", explanation: "" },
+  { title: "308", description: "", explanation: "" },
+];
