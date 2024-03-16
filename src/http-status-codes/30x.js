b3c62b1fb18f4b6e2aa4b454c97e7ff317c885ed
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-03-16 19:37:23 -0300

    📝 docs: #1 add 30x explanations

--- src/http-status-codes/30x.js
+++ src/http-status-codes/30x.js
@@ -23,54 +23,67 @@ export const HttpCodes30x = [
     code: "300",
     title: "Multiple Choices",
     description: "Server has given multiple choices to request resource",
-    explanation: ""
+    explanation: "Indicates that you received more options to choose how you'll" +
+      " get the wanted resource. These options can include file formats, extensions" +
+      " and other resource related information."
   },
   {
     code: "301",
     title: "Moved Permanently",
     description: "This and all future requests should be redirect",
-    explanation: ""
+    explanation: "The request resource has been moved permanently. This and all" +
+    " subsequent requests should be made to new endpoint returned by this code."
   },
   {
     code: "302",
     title: "Found (former Moved Temporarily)",
     description: "Indicates that your resource was move to another location",
-    explanation: ""
+    explanation: "Temporary redirect to another resource that do not change current URI." +
+      " In other words, means that this address can be put online again in the future."
   },
   {
     code: "303",
     title: "See other",
     description: "You should make a GET request to another resource",
-    explanation: ""
+    explanation: "When you make a POST/PUT request to a resource, and the server don't want" +
+    " to show you this specific resource, but instead redirects you to another location with" +
+    " some special message, like <i>'See more info at...</i>'"
   },
   {
     code: "304",
     title: "Not Modified",
     description: "The resource you request was not modified since last request",
-    explanation: ""
+    explanation: "If you made a request with <tt>If-Modified-Since</tt> or" +
+    " <tt>If-None-Match</tt> and this data was not modified since then, you" +
+    " will receive this Status Code, indicating that no action should be taken."
   },
   {
     code: "305",
     title: "Use Proxy",
     description: "This and subsequent requests should use proxy",
-    explanation: ""
+    explanation: "You tried to access a resource that is behind a proxy and must be used" +
+    " to access its content. This and subsequent requests must be made by a proxy. For security" +
+    " reasons this code is not being largely used and respected for some browsers."
   },
   {
     code: "306",
     title: "Switch Proxy (deprecated)",
     description: "Originally meant that you should issue your requests through a proxy",
-    explanation: ""
+    explanation: "On previous RFC versions this code was defined and now it's reserved."
   },
   {
     code: "307",
     title: "Temporary redirect",
     description: "You should issue the exact same request to another URI",
-    explanation: ""
+    explanation: "Server is telling you that this request is valid, but temporarily" +
+    " allocated in another location. This code also indicates that you MUST NOT change" +
+    " the method used to make this request."
   },
   {
     code: "308",
     title: "Permanent Redirect",
     description: "This and all future requests should be redirect (for same HTTP methods)",
-    explanation: ""
+    explanation: "A very young status, indicates that this and future resources should be found" +
+    " at another location, and this redirection can be cached, since it's permanent."
   },
 ];
