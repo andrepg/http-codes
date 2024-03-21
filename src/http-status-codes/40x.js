c15d37317bd75da4123eb02aa8bfeec0e03d74d9
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-03-20 22:09:17 -0300

    🧩 #1 include 40x http explanations - part 1

--- src/http-status-codes/40x.js
+++ src/http-status-codes/40x.js
@@ -23,103 +23,123 @@ export const HttpCodes40x = [
     code: "400",
     title: "Bad Request",
     description: "Server can not process this due to a malformed request",
-    explanation: ""
+    explanation: "You should review how your request are formed and if have some " +
+      " syntax problem, is invalid and something else."
   },
   {
     code: "401",
     title: "Unauthorized",
     description: "You need to be authenticated to access this specific resource",
-    explanation: ""
+    explanation: "An authentication is required before you access this resource. " +
+      "You should try login or request correct credentials to someone."
   },
   {
     code: "402",
     title: "Payment Required (future use)",
     description: "Made to process digital transactions, or indicate a paywall on HTTP",
-    explanation: ""
+    explanation: "Not standardized response code, can be used to indicated that your request " +
+      "it's behind some kind of paywall or request digital payment to move on."
   },
   {
     code: "403",
     title: "Forbidden",
     description: "Your request is correct, but this action was forbidden to be made on server",
-    explanation: ""
+    explanation: "Even if you are correctly authenticated, something on server forbids you to " +
+      "finish this action. Access is related with some application logic."
   },
   {
     code: "404",
     title: "Not Found",
     description: "You made a request and server was not able to found the resource",
-    explanation: ""
+    explanation: "Whatever you were looking for it's not available anymore. There's no " +
+      "indication that content was moved (as 302) nor authentication problems. " +
+      "<b>Resource does not exists.</b>"
   },
   {
     code: "405",
     title: "Method Not Allowed",
-    description: "The request made is valid, except for your HTTP method (GET, POST, etc.)",
-    explanation: ""
+    description: "The request made is valid, except for your HTTP method",
+    explanation: "This resource really existis. But you are using a wrong method (GET, POST, etc.). " +
+      "Change it and try again to see if it works."
   },
   {
     code: "406",
     title: "Not Acceptable",
     description: "The resource can respond you only with a media type that you does not accept",
-    explanation: ""
+    explanation: "In your header you request as a response some mime-type that server can not " +
+      "generate. Something related with Accept or Accept-*."
   },
   {
     code: "407",
     title: "Proxy Authentication Required",
     description: "The client must authenticate through a proxy before doing this request",
-    explanation: ""
+    explanation: "Your request must be made through an authenticated proxy. So you must " +
+      "issue a request to this server first before succeed in the current request."
   },
   {
     code: "408",
     title: "Request Timeout",
     description: "Your request has timed out. The server has dropped and you should try again",
-    explanation: ""
+    explanation: "This response indicates that you made no request in a defined interval and " +
+      "the server will terminate your connection for now."
   },
   {
     code: "409",
     title: "Conflict",
     description: "The change request will incurr in a conflict with already requested change",
-    explanation: ""
+    explanation: "You try to submit some type of data that will conflict with what the server " +
+      "already have online. This occurs, for example, when you do an upload with a lower version of the file."
   },
   {
     code: "410",
     title: "Gone",
     description: "Your request simply is no longer available. Try it again",
-    explanation: ""
+    explanation: "The access to this resource is no longer available. This can be " +
+      "a permanent condition. If it's temporary, server should return 404."
   },
   {
     code: "411",
     title: "Length Required",
     description: "Your request does not fit minimum length requirements from server",
-    explanation: ""
+    explanation: "The server refuses to accept your request without a <tt>Content-Length<tt> " +
+      "header before you make this request again."
   },
   {
     code: "412",
     title: "Precondition Failed",
     description: "The server does not meed preconditions requested by client",
-    explanation: ""
+    explanation: "If your request does not met a condition, such as a " +
+      "<tt>If-Unmodified-Since</tt> header. In this case not was modified and you " +
+      "should correct your request before issue again."
   },
   {
     code: "413",
     title: "Payload Too Large (former Request Entity Too Large)",
     description: "The request is larger than server can process",
-    explanation: ""
+    explanation: "Most frequent in submissions, means that your request exceeds maximum " +
+      "allowed upload size. You should split or lower you request size."
   },
   {
     code: "414",
     title: "URI Too Long",
     description: "The URI requested is longer than server can process",
-    explanation: ""
+    explanation: "You did a GET request with an URI too long to process in the server. " +
+      "Maybe was meant to be a POST or you should use a short URI."
   },
   {
     code: "415",
     title: "Unsupported Media Type",
     description: "The request has a media type which server can not process",
-    explanation: ""
+    explanation: "You made a request with invalid <tt>Content-Type</tt> header " +
+      "and, therefore your request can not be processed."
   },
   {
     code: "416",
     title: "Range Not Satisfiable",
     description: "Your made a partial request that can not be delivered",
-    explanation: ""
+    explanation: "This request contains a partial solicitation that could not be " +
+      "processed. Either because its sintatically wrong or because our <tt>Range</tt> " +
+      "is out of scope."
   },
   {
     code: "417",
